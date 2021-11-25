a1334375f4fdb3fb01c1ec76e56a565ce4e11773
ANNA BEATRIZ FLORENTINO SHINZATO <anna.shinzato@fatec.sp.gov.br>
2021-11-25 16:08:03 -0300

    teste 1999

--- src/main/java/sysconstucao/crm/controller/Security.java
+++ src/main/java/sysconstucao/crm/controller/Security.java
@@ -20,7 +20,7 @@ public class Security implements WebMvcConfigurer {
 	        return new WebMvcConfigurer() {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+	                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
 	            }
 	        };
 	    }
