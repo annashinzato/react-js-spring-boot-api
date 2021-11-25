754ab013cf865ccf4a032a94de0f646a1a8c490e
ANNA BEATRIZ FLORENTINO SHINZATO <anna.shinzato@fatec.sp.gov.br>
2021-11-25 16:01:12 -0300

    teste cors

---
+++ src/main/java/sysconstucao/crm/controller/Security.java
@@ -0,0 +1,27 @@
+package sysconstucao.crm.controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class Security implements WebMvcConfigurer {
+	
+	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST");
+    }
+	
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() 
+	    {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+	            }
+	        };
+	    }
+}
